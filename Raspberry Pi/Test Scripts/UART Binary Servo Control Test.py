a5477caa68cd23d75b3ebe83e600036a8af9530f
Daniel Dymond <daniel@danieldymond.nz>
2021-05-08 09:56:24 +1200

    Corrected Comment for UART Binary Servo Control script

--- Raspberry Pi/Test Scripts/UART Binary Servo Control Test.py
+++ Raspberry Pi/Test Scripts/UART Binary Servo Control Test.py
@@ -1,14 +1,22 @@
+#   UART Binary Servo Control Test Script
+#   Sends two identical signals (bits 6:0) for the two servos (identified with bit 7)
+#   for the FPGA to general two PWM signals to drive each servo.
+#   Daniel Dymond 2021
+
 #!/usr/bin/env python3
 import serial
 import time
 if __name__ == '__main__':
+
+    # Hook up to UART 2 (GPIO 0/1)
     ser2 = serial.Serial('/dev/ttyAMA1', 9600, timeout=1)
-    ser3 = serial.Serial('/dev/ttyAMA2', 9600, timeout=1)
     ser2.flush()
-    ser3.flush()
+
+    # Initial position of 10
     pos = 10
     delay = 0
     
+    # Loop forever
     while True:
 
         if (delay > 2):
@@ -26,16 +34,13 @@ if __name__ == '__main__':
         byteY = [pos + 128]
         byteToTxX = bytearray(byteX)
         byteToTxY = bytearray(byteY)
+        
         # Show verbose
         print("Tx'd: X=" + str(pos) + ", Y=" + str(pos + 128))
 
-        # Send and Recieve via UART2/3
+        # Send via UART2
         size = ser2.write(byteToTxX)
         size = ser2.write(byteToTxY)
-        #byteFrRx = ser3.readline().decode('utf-8').rstrip()
-
-        # Show verbose
-        #print("Rx'd: " + str(size) + " with " + byteFrRx)
 
         # Pause for 1s
         time.sleep(1)
