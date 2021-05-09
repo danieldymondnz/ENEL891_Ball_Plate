f40e39ebb99ab822bbd6f46c077b00726e722c80
Daniel Dymond <daniel@danieldymond.nz>
2021-05-09 17:47:28 +1200

    Created UART Servo Controller Base

---
+++ Raspberry Pi/UART_Servo_Controller/UART_Servo_Controller.py
@@ -0,0 +1,21 @@
+# Servo Controller
+
+class UART_Servo_Controller:
+
+    # Function to handle X Servo Angle and Control Ouputs
+    def sendXServo(servoXAngle):
+        pass
+
+    # Function to handle Y Servo Angle and Control Outputs    
+    def sendYServo(servoYAngle):
+        pass
+
+    # Private methods to be set private
+
+    # Converts a Given Angle into the 7-bit value required by the FPGA
+    def convertAngle(angle):
+        pass
+
+    # Performs the UART TX for some given data
+    def uartTX(data):
+        pass
