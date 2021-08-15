07ac020ed7421beef241a2720d69cddc0b266fcc
Daniel Dymond <daniel@danieldymond.nz>
2021-08-16 10:45:49 +1200

    Converted SPAS to Thread, added timing from UART

--- Main Application/Backend/ServoPlateAugmentationSystem.py
+++ Main Application/Backend/ServoPlateAugmentationSystem.py
@@ -1,6 +1,27 @@
+import threading
+import time
 
-class ServoPlateAugmentationSystem(object):
+class ServoPlateAugmentationSystem(threading.Thread):
 
+    # Constants
+    MAX_ANGLE = 15          # Maximum Delfection Angle
+    MAX_DELTA_ANGLE = 5     # Maximum Rate of change of Deflection Angle / sec
+
+    def __init__(self, baud, bits):
+        threading.Thread.__init__(self)
+        self.baudDelay = ServoPlateAugmentationSystem.__calculateDelay__(baud, bits)
+        self.framePerSec = baud / bits
+
+    @staticmethod
+    def __calculateDelay__(baud, bits):
+        '''
+        Calculate the delay used based on the Baud Rate and number of Bits per packet. 
+        Method is able to be modified further to add/remove delays to compesnate for python execution.
+        '''
+        period = 1 / (baud / bits)
+        return period
+
+    
     def plateAugmentation(rawAngle):
         
         newAngle = rawAngle
