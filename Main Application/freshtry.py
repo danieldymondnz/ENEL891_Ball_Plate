9ab7de1096bfa0f5be3a08732e92c921067dc551
saraknz <69064718+saraknz@users.noreply.github.com>
2021-09-01 13:31:10 +1200

    GUI backend setup file created. Basic functionality of btns to navigate pages and indicate selections has been setup

---
+++ Main Application/freshtry.py
@@ -0,0 +1,174 @@
+
+
+import sys
+import numpy as np
+import cv2 as cv
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui as qtg
+
+from freshgui import Ui_MainWindow
+
+class ballgui(qtw.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.main_pg)
+
+        # Main Menu btns to navigate to another page
+        self.ui.btn_main_position.clicked.connect(self.showPosition_pg)
+        self.ui.btn_main_pattern.clicked.connect(self.showPattern_pg)
+        self.ui.btn_main_joystick.clicked.connect(self.showJoystick_pg)
+
+        # Menu buttons on each page, to return to Main Menu
+        self.ui.btn_pos_menu.clicked.connect(self.showMain_pg)
+        self.ui.btn_patt_menu.clicked.connect(self.showMain_pg)
+        self.ui.btn_joystick_menu.clicked.connect(self.showMain_pg)
+
+        # Position page btn event set up
+        self.ui.btn_pos_center.clicked.connect(self.setup_posCenter)
+        self.ui.btn_pos_position.clicked.connect(self.setup_posPoint)
+        self.ui.btn_pos_reset.clicked.connect(self.setup_posReset)
+
+        # Patterns page btn event set up
+        self.ui.btn_patt_rectangle.clicked.connect(self.setup_pattRectangle)
+        self.ui.btn_patt_circle.clicked.connect(self.setup_pattCircle)
+        self.ui.btn_patt_infinity.clicked.connect(self.setup_pattInfinity)
+        self.ui.btn_patt_center.clicked.connect(self.setup_pattCenter)
+        self.ui.btn_patt_reset.clicked.connect(self.setup_pattReset)
+
+        # Joystick page btn event set up
+
+    
+
+    def showMain_pg(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.main_pg)  
+        
+    def showPosition_pg(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.position_pg)
+        self.ui.btn_pos_center.setStyleSheet(neutralbtn)
+        self.ui.btn_pos_position.setStyleSheet(neutralbtn)
+        
+    def showPattern_pg(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pattern_pg)
+        self.ui.btn_patt_rectangle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_circle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_infinity.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_center.setStyleSheet(neutralbtn)
+
+    def showJoystick_pg(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.joystick_pg)
+        self.ui.btn_stick_position.setStyleSheet(neutralbtn)
+        self.ui.btn_stick_angle.setStyleSheet(neutralbtn)
+
+    def setup_posCenter(self):
+        self.ui.btn_pos_center.setStyleSheet(clickedbtn)
+        self.ui.btn_pos_position.setStyleSheet(neutralbtn)
+        self.ui.btn_pos_menu.setEnabled(False)
+        self.ui.btn_pos_menu.setStyleSheet(disablebtn)
+
+    def setup_posPoint(self):
+        self.ui.btn_pos_position.setStyleSheet(clickedbtn)
+        self.ui.btn_pos_center.setStyleSheet(neutralbtn)
+        self.ui.btn_pos_menu.setEnabled(False)
+        self.ui.btn_pos_menu.setStyleSheet(disablebtn)
+
+    def setup_posReset(self):
+        self.ui.btn_pos_menu.setEnabled(True)
+        self.ui.btn_pos_menu.setStyleSheet(whitebtn)
+        self.ui.btn_pos_center.setStyleSheet(neutralbtn)
+        self.ui.btn_pos_position.setStyleSheet(neutralbtn)
+
+    def setup_pattRectangle(self):
+        self.ui.btn_patt_rectangle.setStyleSheet(clickedbtn)
+        self.ui.btn_patt_circle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_infinity.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_center.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_menu.setEnabled(False)
+        self.ui.btn_patt_menu.setStyleSheet(disablebtn)
+
+    def setup_pattCircle(self):
+        self.ui.btn_patt_circle.setStyleSheet(clickedbtn)
+        self.ui.btn_patt_rectangle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_infinity.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_center.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_menu.setEnabled(False)
+        self.ui.btn_patt_menu.setStyleSheet(disablebtn)
+
+    def setup_pattInfinity(self):
+        self.ui.btn_patt_infinity.setStyleSheet(clickedbtn)
+        self.ui.btn_patt_rectangle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_circle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_center.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_menu.setEnabled(False)
+        self.ui.btn_patt_menu.setStyleSheet(disablebtn)
+
+    def setup_pattCenter(self):
+        self.ui.btn_patt_center.setStyleSheet(clickedbtn)
+        self.ui.btn_patt_rectangle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_circle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_infinity.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_menu.setEnabled(False)
+        self.ui.btn_patt_menu.setStyleSheet(disablebtn)
+    
+    def setup_pattReset(self):
+        self.ui.btn_patt_menu.setEnabled(True)
+        self.ui.btn_patt_menu.setStyleSheet(whitebtn)
+        self.ui.btn_patt_rectangle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_circle.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_infinity.setStyleSheet(neutralbtn)
+        self.ui.btn_patt_center.setStyleSheet(neutralbtn)
+
+        
+
+
+neutralbtn = """ 
+QPushButton {
+border-width: 2px;
+border-style: outset;
+border-radius: 7;
+padding: 3px;
+border-color: #0c1b33;
+background-color: rgb(189, 213, 234);
+color: #0c1b33;
+}
+"""
+clickedbtn = """
+QPushButton {
+border-width: 2px;
+border-style: outset;
+border-radius: 7;
+padding: 3px;
+border-color: #0c1b33;
+background-color: rgb(79, 178, 134);
+color: #0c1b33;
+}
+"""
+disablebtn = """
+QPushButton {
+border-width: 2px;
+border-color: rgb(179, 179, 179);
+border-style: solid;
+border-radius: 7;
+padding: 3px;
+background-color: #fbfbff;
+}
+"""
+whitebtn = """
+QPushButton {
+border-width: 2px;
+border-style: solid;
+border-radius: 7;
+padding: 3px;
+color: rgb(12, 27, 51);
+border-color: rgb(12, 27, 51);
+background-color: rgb(251, 251, 255);
+}
+"""
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv)
+    main_win = ballgui()
+    main_win.show()
+    sys.exit(app.exec_())
