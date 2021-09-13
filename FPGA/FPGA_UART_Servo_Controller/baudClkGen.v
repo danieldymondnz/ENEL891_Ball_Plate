b9b87200cf7e85213b592ccd9f48b44675f47aa9
Daniel Dymond <daniel@danieldymond.nz>
2021-09-13 12:21:09 +1200

    Modified UART
    Uses 9600 baud/7-bit + 1 identifier/ 0.5 deg resolution

--- FPGA/FPGA_UART_Servo_Controller/baudClkGen.v
+++ FPGA/FPGA_UART_Servo_Controller/baudClkGen.v
@@ -1,8 +1,8 @@
 // baudClkGen 
 // Generates a Baud Clock Rate based upon the 50 MHz Crystal on the DE0-Nano
 // Ball Plate Project - Daniel Dymond, Sara Kinghan
-// Using standardised Baud Rate of 19200 * 16 tick/sample (more than ample samples)
-// 50 MHz / 19200 / 16 / 2 - 1 = 80
+// Using standardised Baud Rate of 9600 * 16 tick/sample (more than ample samples)
+// 50 MHz / 9600 / 16 / 2 - 1 = 162
 
 module baudClkGen (	input clk50MHz,
 							output reg baudClk);
@@ -10,7 +10,7 @@ module baudClkGen (	input clk50MHz,
 		reg [6:0] count;
 		
 		always @ (posedge clk50MHz) begin
-			if (count > 80) begin
+			if (count > 162) begin
 				count = 0;
 				baudClk = !baudClk;
 			end
--- FPGA/FPGA_UART_Servo_Controller/servoPWMDriver.v
+++ FPGA/FPGA_UART_Servo_Controller/servoPWMDriver.v
@@ -2,13 +2,13 @@ module servoPWMDriver( 	input [9:0] position,
 								input clk,
 								output reg pulse );
 	
-	reg [12:0] counter, mark;
+	reg [8:0] counter, mark;
 		
 	always@ (posedge clk)	begin
 	
 			// 360 ticks per frame
 			if (counter > 359)
-				counter = 13'b0;
+				counter = 9'b0;
 			else 
 				counter = counter + 1;
 
@@ -18,8 +18,8 @@ module servoPWMDriver( 	input [9:0] position,
 			else
 				pulse = 1'b0;
 				
-			// Update Mark with new Input Data + 1791
-			mark = position + 1791;
+			// Update Mark with new Input Data + 139
+			mark = position + 139;
 	end
 						
 endmodule 
