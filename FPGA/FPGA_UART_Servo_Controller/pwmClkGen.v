0024999a11792c90ab332be19608f7b3c76f3858
Daniel Dymond <daniel@danieldymond.nz>
2021-09-13 12:21:09 +1200

    Modified UART
    Uses 9600 baud/7-bit + 1 identifier/ 0.5 deg resolution

--- FPGA/FPGA_UART_Servo_Controller/pwmClkGen.v
+++ FPGA/FPGA_UART_Servo_Controller/pwmClkGen.v
@@ -1,19 +1,19 @@
 module pwmClkGen (input clk50MHz,
 						output reg pwmClk);
 						
-	reg [4:0] count;
+	reg [7:0] count;
 		
 		always @ (posedge clk50MHz) begin
 			
 			// Servo operates at 333Hz
-			// For 1024 bits resolution over 40*, totals 4608 ticks/frame
-			// 50 / (4608 * 333 * 2) - 1= 15
-			if (count > 15) begin
-				count = 5'd0;
+			// For 0.5 degree resolution over 180*, totals 360 ticks per frame
+			// 50 / (360 * 333 * 2) - 1 = 208
+			if (count > 208) begin
+				count = 8'd0;
 				pwmClk = !pwmClk;
 			end
 			else
-				count = count + 5'd1;
+				count = count + 8'd1;
 		end
 		
 endmodule 
--- FPGA/FPGA_UART_Servo_Controller/servoSelectController.v
+++ FPGA/FPGA_UART_Servo_Controller/servoSelectController.v
@@ -1,53 +1,21 @@
 module servoSelectController( input [7:0] rxData, input newData,
-										output reg [9:0] xPWM,yPWM);
+										output reg [6:0] xPWM,yPWM);
 						
-	reg [9:0] xPWMStore;
-	reg [9:0] yPWMStore;
-	reg [4:0] highData;
-	reg [4:0] lowData;
-	reg lastServoWrite;
-	reg currServoWrite;
-	
-	initial lastServoWrite = 1'b1;
+	reg [6:0] xPWMStore;
+	reg [6:0] yPWMStore;
 						
 	always @(posedge newData) begin
-	
-		// Set Servo Flag
-		if (rxData[7:5] == 3'b000) begin
-			currServoWrite = 1'b0;
+
+		if (rxData[7] == 1'b0) begin
+			xPWMStore = rxData[6:0]
 		end
 		else begin
-			currServoWrite = 1'b1;
-		end
-		
-		// If last servo write is same as current, then extract data in last 5 positions
-		if (lastServoWrite == currServoWrite) begin
-			lowData = rxData[4:0];
-		end
-		else begin
-			highData = rxData[4:0];
-		end
-		
-		// If both high and low data is determined, write the data to the applicable datastore
-		if (lastServoWrite == currServoWrite) begin
-			
-			if (currServoWrite == 1'b0) begin
-				xPWMStore[9:5] = highData;
-				xPWMStore[4:0] = lowData;
-			end
-			else begin
-				yPWMStore[9:5] = highData;
-				yPWMStore[4:0] = lowData;
-			end
-			
+			yPWMStore = rxData[6:0]
 		end
 		
 		// Regardless, output current values for x & y PWM
 		xPWM = xPWMStore;
 		yPWM = yPWMStore;
-		
-		// Shuffle Servo Flag for next comparision
-		lastServoWrite = currServoWrite;
 			
 	end
 						
